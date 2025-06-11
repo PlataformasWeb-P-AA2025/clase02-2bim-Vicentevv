bbd3a18f62607b51c34771b6a58539634ea6a247
Vicentevv <vicenteval759@gmail.com>
2025-06-11 09:26:32 -0500

    Ejemplo03

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # clase02-2bim
 Captura de pantalla de print(estudiantes)
 ![alt text](image.png)
+![alt text](image-1.png)
+
+Puerto 8000 y 9090
+![alt text](image-2.png)
--- ejemplo00/app1/admin.py
+++ ejemplo00/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Importar las clases del modelo
+from app1.models import Estudiante
+
+admin.site.register(Estudiante)
